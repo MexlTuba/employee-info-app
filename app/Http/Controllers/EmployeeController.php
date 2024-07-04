81878603b2d416a928fd6b573c472fa7be0b3381
Mexl Delver Tuba <90206212+MexlTuba@users.noreply.github.com>
2024-07-04 10:31:37 +0800

    formdata updates

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Employee;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
 
 class EmployeeController extends Controller
 {
@@ -21,30 +22,51 @@ class EmployeeController extends Controller
 
     public function store(Request $request)
     {
+        Log::info('Entered store method');
+        Log::info('Request data: ' . json_encode($request->all()));
+        Log::info('All uploaded files: ' . json_encode($request->allFiles()));
+
+        if ($request->hasFile('photo')) {
+            Log::info('Photo file detected: ' . $request->file('photo')->getClientOriginalName());
+        } else {
+            Log::info('No photo file detected');
+        }
+
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:employees',
             'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
+        Log::info('Validation passed');
+
         $employee = new Employee();
         $employee->name = $request->input('name');
         $employee->email = $request->input('email');
 
         if ($request->hasFile('photo')) {
+            Log::info('Photo file exists');
             $file = $request->file('photo');
             $fileName = time() . '.' . $file->getClientOriginalExtension();
             $filePath = 'employee_photos/' . $fileName;
 
-            Storage::disk('employee_photos')->put($fileName, file_get_contents($file));
-            $employee->photo = $filePath;
+            if (Storage::disk('employee_photos')->put($fileName, file_get_contents($file))) {
+                Log::info('File stored successfully');
+                $employee->photo = $filePath;
+            } else {
+                Log::error('Failed to store the file');
+            }
+        } else {
+            Log::error('No photo file in request');
         }
 
         $employee->save();
 
-        return redirect()->route('employees.index')->with('success', 'Employee created 
-successfully.');
+        return redirect()->route('employees.index')->with('success', 'Employee created successfully.');
     }
+
+
+
 
     public function show($id)
     {
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
+use Illuminate\Support\Facades\Log;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,8 @@ Route::get('/employees', [EmployeeController::class, 'index'])->name('employees.
 Route::get('/employees/create', [EmployeeController::class, 'create'])->name('employees.create');
 Route::post('/employees', [EmployeeController::class, 'store'])->name('employees.store');
 Route::get('/employees/{id}', [EmployeeController::class, 'show'])->name('employees.show');
+
+Route::get('/test-log', function () {
+    Log::info('Test log message');
+    return 'Log message written';
+});
